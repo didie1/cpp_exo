391db930fc3ea57642c42c2169d88dbfe7776d3b
Elodie Ratovoherinjanahary <elodie.ratovoherinjanahary@epita.fr>
2022-09-28 13:55:58 +0200

    after review

--- find_motive/main.cpp
+++ find_motive/main.cpp
@@ -3,14 +3,17 @@
 #include<fstream>
 #include<iostream>
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
+int main(int argc, char **argv)
+{
+    if (argc != 3) 
+    {
         std::cout << "Please provide both filename and motive" << std::endl;
         return 0;
     }
     std::ifstream file(argv[1]);
 
-    if (file.fail()) {
+    if (file.fail()) 
+    {
         std::cout << "The file " << argv[1] << " could not be opened." << std::endl;
         return 1;
     }
@@ -20,21 +23,26 @@ int main(int argc, char **argv) {
 
     size_t count = 0;
 
-    while (std::getline(file, line)) {
-        if (line == "") {
+    while (std::getline(file, line))
+    {
+        if (line == "")
+        {
             continue;
         }
         size_t pos = 0;
         std::string match;
-        while (((pos = line.find(' ')) != std::string::npos)) {
+        while (((pos = line.find(' ')) != std::string::npos)) 
+        {
             match = line.substr(0, pos);
-            if (match.find(motive) != std::string::npos) {
+            if (match.find(motive) != std::string::npos) 
+            {
                 count++;
             }
             line.erase(0, pos + 1);
         }
 
-        if (line != "" && (line.find(motive) != std::string::npos)) {
+        if (line != "" && (line.find(motive) != std::string::npos))
+        {
             count++;
         }
     }
